70bf9ce9312c5f56bab49706910b8cb9e8e4d5d3
Thiago Henrique Felix Cândido Ribeiro Conceição <ricandido5@gmail.com>
2024-07-10 18:55:32 -0300

    adicionando prototipos das telas: cadastro, login e perfil

---
+++ solosmart/resources/views/cadastro.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro</title>
+</head>
+<body>
+    <div>
+        <div>
+            <form action="" method="GET">
+                <label for="nome">Nome</label>
+                <input type="text" id="nome" name="nome">
+                <label for="email">E-mail</label>
+                <input type="email" id="email" name="email">
+                <label for="telefone">Telefone</label>
+                <input type="tel" id="telefone" name="telefone">
+                <label for="senha">Senha</label>
+                <input type="password" id="senha" name="senha">
+                <label for="csenha">Confirmar senha</label>
+                <input type="password" id="csenha" name="csenha">
+                <input type="submit" value="Cadastrar">
+            </form>
+        </div>
+    </div>
+</body>
+</html>
---
+++ solosmart/resources/views/login.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Logar</title>
+</head>
+<body>
+    <div>
+        <div>
+            <form action="" method="GET">
+                <label for="email">E-mail</label>
+                <input type="email" id="email" name="email">
+                <label for="senha">Senha</label>
+                <input type="password" id="senha" name="senha">
+                <input type="submit" value="Entrar">
+            </form>
+        </div>
+    </div>
+</body>
+</html>
