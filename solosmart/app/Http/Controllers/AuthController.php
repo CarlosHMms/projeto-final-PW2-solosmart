9e4fa9c8c1d0c52809c9672b9911c8e9855c58fb
Carlos Henrique Mota <carloshmmg2@gmail.com>
2024-07-27 17:40:17 -0300

    validar login

--- solosmart/app/Http/Controllers/AuthController.php
+++ solosmart/app/Http/Controllers/AuthController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -11,9 +13,19 @@ class AuthController extends Controller
         return view('login');
     }
 
-    public function autenticacao(Request $request)
-    {
-        return "esperando a lógica";
+    public function autenticacao(Request $request){
+
+        //Verificar autenticação
+        $user = User::where('email', $request->input('email'))->first();
+        var_dump($user);
+        if(!$user){
+            return redirect()->route("login")->with('error','Email or password invalid');
+        }
+        if (!password_verify($request->input("senha"), $user->password)) {
+            return redirect()->route("login")->with('error','Email or password invalid');
+        }else{
+            return redirect()->route('home')->with('success','');
+        }
     }
 
     public function cadastro()
