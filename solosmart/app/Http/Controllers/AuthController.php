f208774e4fb44785732f941b46e8ea2e43d5411b
Carlos Henrique Mota <carloshmmg2@gmail.com>
2024-07-29 22:04:58 -0300

    inicio do cadastro e persistencia no banco

--- solosmart/app/Http/Controllers/AuthController.php
+++ solosmart/app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class AuthController extends Controller
 {
@@ -12,26 +13,36 @@ class AuthController extends Controller
     {
         return view('login');
     }
+    public function cadastro(){
+        return view('cadastro');
+    }
 
     public function autenticacao(Request $request){
 
         //Verificar autenticação
         $user = User::where('email', $request->input('email'))->first();
-        $hash = password_hash($user->password, PASSWORD_DEFAULT);
         if(!$user){
             echo 'negado user';
             return redirect()->route("login")->with('error','Email or password invalid');
         }
-        if (!password_verify($request->input("senha"), $hash)) {
+        if (!password_verify($request->input("senha"), password_hash($user->password, PASSWORD_DEFAULT))) {
             return redirect()->route("login")->with('error','Email or password invalid');
         }else{
             return redirect()->route('home')->with('success','');
         }
     }
 
-    public function cadastro(Request $request){
+    public function cadastrarUsuario(Request $request){
     {
-        $newUser = User::where('email', $request->input('email'))->first();
+        // Validação meia boca e cadastro de usuário (não está funcionando)
+        if(isset($request)){
+            DB::table("users")->insert([
+                'name' => $request->input('name'),
+                'email' => $request->input('email'),
+                'password' => password_hash($request->input('senha'), PASSWORD_DEFAULT)
+            ]);
+        }
+        
     }
 //
 }
