ba5df910563d7b49e917031e61407618ba9c37db
Thiago Henrique Felix Cândido Ribeiro Conceição <ricandido5@gmail.com>
2024-07-31 22:00:14 -0300

    estilizando caixas de alertas ao cadastrar

--- solosmart/resources/views/cadastro.blade.php
+++ solosmart/resources/views/cadastro.blade.php
@@ -10,18 +10,42 @@
 <body class="h-screen content-center items-center justify-items-center" style="background-color:#6D4C3D">
     <!--Thiagão dps estiliza essa caixa aqui-->
     @if (session('success'))
-        <div class="bg-[#31E981] p-3 text-white text-center">
-            {{ session('success') }}
+        <div class="w-full flex justify-center">
+            <div class="flex items-center p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400 justify-center md:w-[500px] w-full absolute" role="alert">
+                <svg class="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
+                </svg>
+                <span class="sr-only">Info</span>
+                <div>
+                    <span class="font-medium">{{ session('success')}}</span>
+                </div>
+            </div>
         </div>
     @endif
     @if (session('error'))
-        <div class="bg-[#FF0000] p-3 text-white text-center">
-            {{ session('error') }}
+        <div class="w-full flex justify-center">
+            <div class="flex items-center p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400 justify-center md:w-[500px] w-full absolute" role="alert">
+                <svg class="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
+                </svg>
+                <span class="sr-only">Info</span>
+                <div>
+                    <span class="font-medium">{{ session('error')}}</span>
+                </div>
+            </div>
         </div>
     @endif
     @if (session('errorEmailAlready'))
-        <div class="bg-[#FF0000] p-3 text-white text-center">
-            {{ session('errorEmailAlready') }}
+        <div class="w-full flex justify-center">
+            <div class="flex items-center p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400 justify-center md:w-[500px] w-full absolute" role="alert">
+                <svg class="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
+                </svg>
+                <span class="sr-only">Info</span>
+                <div>
+                    <span class="font-medium">{{ session('errorEmailAlready')}}</span>
+                </div>
+            </div>
         </div>
     @endif
     
